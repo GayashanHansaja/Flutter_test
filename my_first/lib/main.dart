62a00cfa101cbc99c1890e0a818de1ed8909d6ee
IT22171856 <142132191+IT22171856@users.noreply.github.com>
2025-01-26 20:27:19 +0530

    assets,padding,sizedbox

--- my_first/lib/main.dart
+++ my_first/lib/main.dart
@@ -10,14 +10,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My First App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My First App"),
-          backgroundColor: Colors.blue,
-        ),
-        body: const Icon(Icons.add),
-      ),
-    );
+        title: "My First App",
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("My First App"),
+              backgroundColor: Colors.blue,
+            ),
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset("assets/dani.jpg", height: 200),
+                ),
+                SizedBox(height: 50),
+                Image.asset("assets/cyberpunk.jpg", height: 200),
+              ],
+            )
+            /* Center(
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.red,
+              ),
+              child: Text("Hello"),
+            ),
+          )), */
+            ));
   }
 }
